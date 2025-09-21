98ad034d52805610e6ee0daa01a42362d4208093
Abhayrajdunerya <abhayrajdunerya@gmail.com>
2025-09-21 17:04:13 +0530

    Show Top Questions and Tags

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -29,6 +29,7 @@ import {
 } from "@/types/action";
 import ROUTES from "@/constants/routes";
 import { revalidatePath } from "next/cache";
+import dbConnect from "../mongoose";
 
 export async function createQuestion(
   params: CreateQuestionParams
@@ -325,6 +326,25 @@ export async function incrementViews(
     return {
       success: true,
       data: { views: question.views },
+    };
+  } catch (error) {
+    return handleError(error) as ErrorResponse;
+  }
+}
+
+export async function getHotQuestions(): Promise<
+  ActionResponse<QuestionType[]>
+> {
+  try {
+    await dbConnect();
+
+    const questions = await Question.find()
+      .sort({ views: -1, upvotes: -1 })
+      .limit(5);
+
+    return {
+      success: true,
+      data: JSON.parse(JSON.stringify(questions)),
     };
   } catch (error) {
     return handleError(error) as ErrorResponse;
